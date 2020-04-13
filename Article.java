d847f953fda43666b8786cff9bc428b9a6eaab1b
elkhoukh-soumia <elkhoukhsoumia@gmail.com>
2020-04-13 20:28:10 +0200

    initial commit

---
+++ Article.java
@@ -0,0 +1,25 @@
+
+package exercice4;
+
+
+public class Article {
+    private float prix;
+    private int code;
+
+    public float getPrix() {
+        return prix;
+    }
+
+    public void setPrix(float prix) {
+        this.prix = prix;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+    
+}
---
+++ ListeArticle.java
@@ -0,0 +1,16 @@
+
+package exercice4;
+
+import java.util.List;
+
+
+public class ListeArticle {
+    
+    List<Article> list ;
+
+    
+    public float CalculeTotal(){
+        System.out.println("je calcule le  prix total et  je le rends");
+        return (float) 100.0;
+    }
+}
